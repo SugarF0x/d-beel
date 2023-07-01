11810e9f67c2c7c624a3b3407c1bf61bac1ebdf8
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-01 19:43:25 +0300

    add auth protected home page

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth']
+})
+
+const { data, signOut } = useAuth()
+</script>
+
+<template>
+  <h1>i am a protected index page</h1>
+
+  <pre class="data">{{ data }}</pre>
+
+  <v-btn @click="signOut">logout</v-btn>
+</template>
+
+<style scoped lang="scss">
+.data {
+  margin: 24px 0;
+  padding: 16px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 8px;
+}
+</style>
