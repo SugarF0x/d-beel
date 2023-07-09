5ef073b0afb538719c00b88a5b9b28cdc714d662
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-09 13:23:35 +0300

    write a small workaroudn instead

--- modules/prepare-for-yandex-functions.ts
+++ modules/prepare-for-yandex-functions.ts
@@ -8,8 +8,6 @@ export default defineNuxtModule({
 
       rmSync('./.output/server/node_modules', { recursive: true, force: true })
 
-      renameSync('./.output/server/index.mjs', './.output/server/index.js')
-
       const serverPackage = JSON.parse(readFileSync('./.output/server/package.json', 'utf-8'))
 
       serverPackage.type = "module"
@@ -17,6 +15,20 @@ export default defineNuxtModule({
       delete serverPackage.bundledDependencies
 
       writeFileSync('./.output/server/package.json', JSON.stringify(serverPackage, null, 2))
+
+      writeFileSync('./.output/server/index.js', `
+        import { handler } from "./index.mjs"
+        
+        async function wrapper(event, context) {
+          if (event.body && event.isBase64Encoded) event.body = atob(event.body)
+          
+          const response = handler(event, context)
+          
+          return response
+        }
+        
+        export { wrapper as handler }
+      `)
     }
   }
 })
