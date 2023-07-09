2cc4b3720ddfd659755d4ec00cfc46cbcd099ad1
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-09 18:38:42 +0300

    add todos

--- modules/prepare-for-yandex-functions.ts
+++ modules/prepare-for-yandex-functions.ts
@@ -6,6 +6,8 @@ export default defineNuxtModule({
     'close': (nuxt) => {
       if (process.env.NODE_ENV !== 'production') return
 
+      // TODO: figure out a way to post process all the imports to point directly to node_modules
+
       rmSync('./.output/server/node_modules', { recursive: true, force: true })
 
       const serverPackage = JSON.parse(readFileSync('./.output/server/package.json', 'utf-8'))
@@ -15,6 +17,8 @@ export default defineNuxtModule({
       delete serverPackage.bundledDependencies
 
       writeFileSync('./.output/server/package.json', JSON.stringify(serverPackage, null, 2))
+
+      // TODO: figure out a way of better managing this template
 
       writeFileSync('./.output/server/index.js', `
         import { handler } from "./index.mjs"
