31894dd04e5b9407975b6d3f9091cda4a632e28c
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-11 11:44:16 +0300

    extract wrapper js into a top level constant

--- modules/prepare-for-yandex-functions.ts
+++ modules/prepare-for-yandex-functions.ts
@@ -20,25 +20,27 @@ export default defineNuxtModule({
 
       // TODO: figure out a way of better managing this template
 
-      writeFileSync('./.output/server/index.js', `
-        import { handler } from "./index.mjs"
-        
-        async function wrapper(event, context) {
-          if (event.body && event.isBase64Encoded) event.body = atob(event.body)
-          
-          const response = await handler(event, context)
-          
-          if (response.headers['set-cookie']) {
-            response.multiValueHeaders ??= {}
-            response.multiValueHeaders['set-cookie'] = response.headers['set-cookie'].split(/(.*?),(.*?)($|,(?! ))/).filter(Boolean)
-            delete response.headers['set-cookie']
-          }
-          
-          return response
-        }
-        
-        export { wrapper as handler }
-      `)
+      writeFileSync('./.output/server/index.js', wrapperJs)
     }
   }
 })
+
+const wrapperJs: string =
+`import { handler } from "./index.mjs"
+        
+async function wrapper(event, context) {
+  if (event.body && event.isBase64Encoded) event.body = atob(event.body)
+  
+  const response = await handler(event, context)
+  
+  if (response.headers['set-cookie']) {
+    response.multiValueHeaders ??= {}
+    response.multiValueHeaders['set-cookie'] = response.headers['set-cookie'].split(/(.*?),(.*?)($|,(?! ))/).filter(Boolean)
+    delete response.headers['set-cookie']
+  }
+  
+  return response
+}
+
+export { wrapper as handler }
+`
