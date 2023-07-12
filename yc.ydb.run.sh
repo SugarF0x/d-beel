c61d75a7fd41e68df212d72883ccd1a25bd33d93
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-12 19:41:00 +0300

    add local ydb instance

---
+++ yc.ydb.run.sh
@@ -0,0 +1,7 @@
+docker run -d --rm --name ydb-local -h localhost \
+  -p 2135:2135 -p 8765:8765 -p 2136:2136 \
+  -v $(pwd)/ydb_certs:/ydb_certs -v $(pwd)/ydb_data:/ydb_data \
+  -e YDB_DEFAULT_LOG_LEVEL=NOTICE \
+  -e GRPC_TLS_PORT=2135 -e GRPC_PORT=2136 -e MON_PORT=8765 \
+  -e YDB_USE_IN_MEMORY_PDISKS=true \
+  cr.yandex/yc/yandex-docker-local-ydb:latest
