9971f7328018de45a7911ba992e91b2dbff24b21
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-19 20:42:48 +0300

    add hots main page

--- pages/hots.vue
+++ pages/hots.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-
+useHead({
+  title: 'Дебилы Шторма'
+})
 </script>
 
 <template>
-  i am a TOBE hots page
+  <div class="wrapper">
+    <v-img class="hero" src="/img/hots/hero.png" max-width="800" />
+  </div>
 </template>
 
 <style scoped lang="scss">
+.wrapper {
+  flex: 1;
+  background: url('/img/hots/background.jpg') center;
+}
 
+.hero {
+  margin: 0 auto;
+}
 </style>
