bd27d25a900df0e8d318265c439f7a6966d5ccea
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-19 20:32:15 +0300

    add home page with hots page link

---
+++ pages/hots.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  i am a TOBE hots page
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
 const { data, signOut } = useAuth()
+
+const authButtonText = computed(() => !data.value ? 'Войти' : 'Выйти')
+
+function handleAuth() {
+  if (!!data.value) navigateTo('/login')
+  else signOut()
+}
 </script>
 
 <template>
-  <h1>i am a protected index page</h1>
+  <div class="wrapper">
+    <v-btn class="auth-button" @click="handleAuth">{{ authButtonText }}</v-btn>
 
-  <pre class="data">{{ data }}</pre>
-
-  <v-btn @click="signOut">logout</v-btn>
+    <app-card-tilt @click="navigateTo('/hots')">
+      <v-img src="/img/hots/logo.png" width="128" height="128" />
+    </app-card-tilt>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.data {
-  margin: 24px 0;
-  padding: 16px;
-  background-color: rgba(0, 0, 0, 0.5);
-  border-radius: 8px;
+.wrapper {
+  flex: 1;
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.auth-button {
+  position: absolute;
+  top: 24px;
+  right: 24px;
 }
 </style>
