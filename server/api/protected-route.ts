c115e8689da1747df717c52bc22235bdf5c9ed0f
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-20 20:17:24 +0300

    remove unused api route

--- server/api/protected-route.ts
+++
@@ -1,8 +0,0 @@
-import useAuth from "~/server/utils/useAuth"
-
-export default defineEventHandler(async (event) => {
-  const session = await useAuth(event)
-
-  console.log(session)
-  return { response: { data: 'pog' } }
-})
