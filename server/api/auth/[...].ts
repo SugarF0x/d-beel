174c57f4e7e801df9f907281b6ddb1afe40f2c89
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-20 21:09:36 +0300

    bundle username into jwt & session data

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,5 +1,6 @@
 import { NuxtAuthHandler } from "#auth"
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { User } from "next-auth"
 
 // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
 const Provider = CredentialsProvider.default ?? CredentialsProvider
@@ -13,16 +14,25 @@ export default NuxtAuthHandler({
         username: { label: 'Username', type: 'text', placeholder: '(hint: jsmith)' },
         password: { label: 'Password', type: 'password', placeholder: '(hint: hunter2)' }
       },
-      authorize (credentials: any) {
-        const user = { id: '1', name: 'J Smith', username: 'jsmith', password: 'hunter2' }
+      authorize(credentials: any) {
+        const user = { id: '1', username: 'jsmith', password: 'hunter2' }
 
-        if (credentials?.username === user.username && credentials?.password === user.password) return user
+        if (credentials?.username === user.username && credentials?.password === user.password) return { username: user.username }
 
         console.error('Warning: Malicious login attempt registered, bad credentials provided')
         return null
       }
     })
   ],
+  callbacks: {
+    async jwt({ token, user }){
+      if (user) token.user = user
+      return token
+    },
+    async session({ token, session }) {
+      return { ...session, user: token.user as User }
+    }
+  },
   pages: {
     signIn: '/login'
   }
