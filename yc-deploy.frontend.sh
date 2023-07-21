5d79bfa58b237d27cb287fd99c6e87e44f1b3333
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-21 22:23:02 +0300

    force index.html update on each deploy

--- yc-deploy.frontend.sh
+++ yc-deploy.frontend.sh
@@ -3,3 +3,7 @@ source ./.env.cloud
 aws --profile yandex \
   s3 --endpoint-url=https://storage.yandexcloud.net \
   sync --size-only --delete ./.output/public s3://"$S3_BUCKET_ID"
+
+aws --profile yandex \
+  s3 --endpoint-url=https://storage.yandexcloud.net \
+  cp ./.output/public/index.html s3://"$S3_BUCKET_ID"
