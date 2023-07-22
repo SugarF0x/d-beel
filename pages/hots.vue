3befc49e60d533be6ae0a57e0dd94a8aed5c10d6
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-22 11:30:49 +0300

    add a few more dummy data entries

--- pages/hots.vue
+++ pages/hots.vue
@@ -1,24 +1,50 @@
 <script setup lang="ts">
-import { HotsPost } from "~/server/ydb/tables/hots_post"
+import { HotsPostRow } from "~/server/ydb/tables/hots_post"
 import { HotsHero } from "~/server/ydb/types/hots/heroes"
 
 useHead({
   title: 'Дебилы Шторма'
 })
 
-const dummyData: HotsPost = {
-  comment: "Ёбаный овощ, на ласт пике видит, что нам нужен танк, а брузер уже имеется, но нет, блядь, я буду играть в молчанку, дрочить себе в anal sex и в итоге всё равно пикать boy next door, после чего флеймит",
-  created_at: new Date("2021-02-26T22:15:20Z"),
-  created_by: null,
-  hero: HotsHero.Sonya,
-  rating: 0,
-  username: "Aknesta"
-}
+const dummyData: HotsPostRow[] = [
+  {
+    comment: "Ёбаный овощ, на ласт пике видит, что нам нужен танк, а брузер уже имеется, но нет, блядь, я буду играть в молчанку, дрочить себе в anal sex и в итоге всё равно пикать boy next door, после чего флеймит",
+    created_at: new Date("2021-02-26T22:15:20Z"),
+    created_by: null,
+    hero: HotsHero.Sonya,
+    rating: 0,
+    username: "Aknesta"
+  },
+  {
+    comment: "Сраный дебил говна, пикнул трейсер, когда та нам нахуй не усралась, в итоге сосала хуй всю игру",
+    created_at: new Date("2021-01-23T13:38:58Z"),
+    created_by: null,
+    hero: HotsHero.Tracer,
+    rating: 1,
+    username: "Alastor"
+  },
+  {
+    comment: "Ебашит как бог на ктз с оверахуевшим винрейтом",
+    created_at: new Date("2021-01-23T13:38:58Z"),
+    created_by: null,
+    hero: HotsHero.Kelthuzad,
+    rating: 5,
+    username: "Albastrel92"
+  },
+]
 </script>
 
 <template>
   <div class="wrapper">
-    <v-img class="hero" src="/img/hots/hero.png" max-width="800" />
+    <v-img class="hero" src="/img/hots/hero.png" />
+
+    <v-container>
+      <v-row>
+        <v-col v-for="(post, index) in dummyData" :key="`${post.username}-${index}`" col="4">
+          <hots-post v-bind="post" />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -30,5 +56,7 @@ const dummyData: HotsPost = {
 
 .hero {
   margin: 0 auto;
+  aspect-ratio: 800 / 350;
+  max-width: 800px;
 }
 </style>
