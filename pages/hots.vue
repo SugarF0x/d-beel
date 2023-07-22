cabf24dbc85d5ca7c433abd9ed67d9fe1be7f34d
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-22 19:43:54 +0300

    fetch actual data

--- pages/hots.vue
+++ pages/hots.vue
@@ -1,37 +1,11 @@
 <script setup lang="ts">
 import { HotsPostRow } from "~/server/ydb/tables/hots_post"
-import { HotsHero } from "~/server/ydb/types/hots/heroes"
 
 useHead({
   title: 'Дебилы Шторма'
 })
 
-const dummyData: HotsPostRow[] = [
-  {
-    comment: "Ёбаный овощ, на ласт пике видит, что нам нужен танк, а брузер уже имеется, но нет, блядь, я буду играть в молчанку, дрочить себе в anal sex и в итоге всё равно пикать boy next door, после чего флеймит",
-    created_at: new Date("2021-02-26T22:15:20Z"),
-    created_by: null,
-    hero: HotsHero.Sonya,
-    rating: 0,
-    username: "Aknesta"
-  },
-  {
-    comment: "Сраный дебил говна, пикнул трейсер, когда та нам нахуй не усралась, в итоге сосала хуй всю игру",
-    created_at: new Date("2021-01-23T13:38:58Z"),
-    created_by: null,
-    hero: HotsHero.Tracer,
-    rating: 1,
-    username: "Alastor"
-  },
-  {
-    comment: "Ебашит как бог на ктз с оверахуевшим винрейтом",
-    created_at: new Date("2021-01-23T13:38:58Z"),
-    created_by: null,
-    hero: HotsHero.Kelthuzad,
-    rating: 5,
-    username: "Albastrel92"
-  },
-]
+const { data } = await useFetch('/api/hots/get-posts-page', { params: { page: 1 } })
 </script>
 
 <template>
@@ -39,7 +13,7 @@ const dummyData: HotsPostRow[] = [
     <v-img class="hero" src="/img/hots/hero.png" />
 
     <v-container class="grid-container">
-      <hots-post v-for="(post, index) in cachedData" :key="`${post.username}-${index}`" v-bind="post" />
+      <hots-post v-for="(post, index) in data as HotsPostRow[]" :key="`${post.username}-${index}`" v-bind="post" />
     </v-container>
   </div>
 </template>
