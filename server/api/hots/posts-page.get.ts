75460c8a2d65652f4cd0dad6272c05f3284578c4
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-25 20:28:29 +0300

    unify three post query routes into one

--- server/api/hots/posts-page.get.ts
+++ server/api/hots/posts-page.get.ts
@@ -2,23 +2,43 @@ import { z } from "zod"
 import { HotsPostRow } from "~/server/ydb/tables/hots_post"
 import { TypedData, TypedValues } from "ydb-sdk"
 import stringToNumber from "~/utils/zod/stringToNumber"
-import { HOTS_POSTS_PER_PAGE } from "~/server/api/hots/total-posts.get"
+import { omit } from "lodash"
+
+export const HOTS_POSTS_PER_PAGE = 12
 
 export default defineEventHandler(async (event) => {
-  const { page } = await getQueryPayload(event, z.object({ page: z.string().transform(stringToNumber) }))
+  const { page, username } = getQueryPayload(event, z.object({
+    page: z.string().transform(stringToNumber),
+    username: z.string().optional()
+  }))
+
   const offset = (page - 1) * HOTS_POSTS_PER_PAGE
 
   return await useYDBSession(async (session) => {
     const { resultSets } = await session.executeQuery(`
       DECLARE $offset AS Uint16;
       DECLARE $limit AS Uint16;
+      DECLARE $username AS Utf8?;
 
-      SELECT * FROM hots_post ORDER BY created_at DESC LIMIT $offset, $limit;
+      SELECT hots_post.*, Count("hots_post.*") OVER() AS total_posts
+      FROM hots_post
+      WHERE IF($username is not NULL, hots_post.username = $username, true)
+      ORDER BY created_at DESC
+      LIMIT $offset, $limit;
     `, {
       "$offset": TypedValues.uint16(offset),
       "$limit": TypedValues.uint16(HOTS_POSTS_PER_PAGE),
+      "$username": TypedValues.optional(username ? TypedValues.utf8(username) : TypedValues.VOID)
     })
 
-    return TypedData.createNativeObjects(resultSets[0]) as unknown as HotsPostRow[]
+    const results = TypedData.createNativeObjects(resultSets[0]) as unknown as Array<HotsPostRow & { total_posts: number }>
+
+    const totalPosts = results[0]?.total_posts ?? 0
+    const posts = results.map<HotsPostRow>(result => omit(result, 'total_posts'))
+
+    return {
+      totalPosts,
+      posts,
+    }
   })
 })
