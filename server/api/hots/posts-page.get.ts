b2b7347355967f271b7334234197abe26ad61dd7
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-25 21:11:35 +0300

    fix optional param query

--- server/api/hots/posts-page.get.ts
+++ server/api/hots/posts-page.get.ts
@@ -2,14 +2,14 @@ import { z } from "zod"
 import { HotsPostRow } from "~/server/ydb/tables/hots_post"
 import { TypedData, TypedValues } from "ydb-sdk"
 import stringToNumber from "~/utils/zod/stringToNumber"
-import { omit } from "lodash"
+import { omit } from "lodash-es"
 
 export const HOTS_POSTS_PER_PAGE = 12
 
 export default defineEventHandler(async (event) => {
   const { page, username } = getQueryPayload(event, z.object({
     page: z.string().transform(stringToNumber),
-    username: z.string().optional()
+    username: z.string().min(1).optional()
   }))
 
   const offset = (page - 1) * HOTS_POSTS_PER_PAGE
@@ -25,11 +25,11 @@ export default defineEventHandler(async (event) => {
       WHERE IF($username is not NULL, hots_post.username = $username, true)
       ORDER BY created_at DESC
       LIMIT $offset, $limit;
-    `, {
+    `, filterOptionalQueryParams({
       "$offset": TypedValues.uint16(offset),
       "$limit": TypedValues.uint16(HOTS_POSTS_PER_PAGE),
-      "$username": TypedValues.optional(username ? TypedValues.utf8(username) : TypedValues.VOID)
-    })
+      "$username": username && TypedValues.optional(TypedValues.utf8(username))
+    }))
 
     const results = TypedData.createNativeObjects(resultSets[0]) as unknown as Array<HotsPostRow & { total_posts: number }>
 
