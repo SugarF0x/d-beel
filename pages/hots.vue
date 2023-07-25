9045821b9142406b5dfde31a84ed4aa692cd39bf
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-25 21:11:52 +0300

    unify query results into a single blob

--- pages/hots.vue
+++ pages/hots.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { HotsPostRow } from "~/server/ydb/tables/hots_post"
-import { useLocalStorage } from "@vueuse/core"
-import { UnwrapRef } from "vue"
-
 useHead({
   title: 'Дебилы Шторма'
 })
 
 const { data: isAuthed } = useAuth()
+
+const searchValue = ref('')
 
 const page = ref(1)
 const debouncedPageIndex = ref(page.value - 1)
@@ -16,48 +14,25 @@ watch([page], () => {
   scrollTo({ top: 0, behavior: "smooth" })
 })
 
-const postsMeta = useLocalStorage('hots-posts-meta', {
-  totalPosts: 0,
-  totalPages: 0
-})
-
-const posts = useLocalStorage<HotsPostRow[][]>('hots-posts', [])
-
-const { pending: isFetching, execute: fetchPosts } = useAsyncData('hots-posts', async () => {
+const { data, pending, execute } = useAsyncData('hots-posts', async () => {
   const index = page.value - 1
 
-  await (async () => {
-    if (posts.value[index]) return posts.value[index]
-
-    posts.value[index] = await $fetch('/api/hots/posts-page', { params: { page: page.value } })
-  })()
+  const results = await $fetch('/api/hots/posts-page', {
+    params: {
+      page: page.value,
+      username: searchValue.value || undefined
+    },
+    retry: false
+  })
 
   debouncedPageIndex.value = index
+  return results
 }, {
   lazy: true,
   watch: [page]
 })
 
-await useAsyncData('hots-posts-meta', async () => {
-  const meta: UnwrapRef<typeof postsMeta> = await $fetch('/api/hots/total-posts')
-
-  if (postsMeta.value.totalPosts !== meta.totalPosts) {
-    // TODO: add data shifting based on total posts delta
-    posts.value = []
-    await fetchPosts()
-  }
-
-  postsMeta.value = meta
-})
-
-const searchValue = ref('')
-watch(searchValue, (value) => {
-  if (!value) searchResults.value = null
-})
-
-const { data: searchResults, status: searchStatus, execute: search } = useAsyncData('hots-username-search', async () => await $fetch('/api/hots/search', { params: { username: searchValue.value } }), { immediate: false })
-
-const isLoading = computed(() => searchStatus.value === 'pending' || isFetching.value)
+const totalPages = computed(() => data.value ? Math.min(data.value.totalPosts / data.value.posts.length, 5) : 1)
 </script>
 
 <template>
@@ -65,20 +40,20 @@ const isLoading = computed(() => searchStatus.value === 'pending' || isFetching.
     <v-img class="hero" src="/img/hots/hero.png" />
 
     <v-container class="controls-container">
-      <v-pagination v-model="page" :length="postsMeta.totalPages" :total-visible="Math.min(postsMeta.totalPages, 5)" :disabled="isLoading || !!searchResults" />
-      <v-text-field v-model="searchValue" hide-details label="Имя дебила" @keyup.enter="() => searchValue.length && search()" />
+      <v-pagination v-model="page" :length="totalPages" :total-visible="Math.min(totalPages, 5)" :disabled="pending" />
+      <v-text-field v-model="searchValue" hide-details label="Имя дебила" @keyup.enter="execute" />
 
       <div class="action">
-        <v-btn color="primary" :disabled="!searchValue.length" @click="search">Поиск</v-btn>
+        <v-btn color="primary" @click="execute">Поиск</v-btn>
         <v-btn color="secondary" :disabled="!isAuthed">Создать</v-btn>
       </div>
     </v-container>
 
-    <v-container class="grid-container" :class="{ loading: isLoading }">
-      <hots-post v-for="(post, index) in searchResults ?? posts[debouncedPageIndex]" :key="`${post.username}-${index}`" v-bind="post" />
+    <v-container class="grid-container" :class="{ loading: pending }">
+      <hots-post v-for="(post, index) in data?.posts ?? []" :key="`${post.username}-${index}`" v-bind="post" />
     </v-container>
 
-    <v-pagination v-model="page" :length="postsMeta.totalPages" :total-visible="Math.min(postsMeta.totalPages, 5)" :disabled="isLoading || !!searchResults" />
+    <v-pagination v-model="page" :length="totalPages" :total-visible="Math.min(totalPages, 5)" :disabled="pending" />
   </div>
 </template>
 
