62d929eb5e8d37ea4bd37174135ec326d93aba9c
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-07-26 23:08:05 +0300

    add forms and unified tabs render data source

--- pages/hots/create.vue
+++ pages/hots/create.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { HotsHero, HotsHeroRole } from "~/server/ydb/types/hots/heroes"
+import { hotsRatingColors } from "~/const/hots/colors"
 
 definePageMeta({ layout: 'hots', middleware: ['auth'] })
 useHead({ title: 'Новый дебил' })
 
-const tabTitles = ['Герой', 'Ревью', 'Публикация']
 const tabIndex = ref(0)
+const tabItems = computed(() => [
+  { text: 'Герой' },
+  { text: 'Ревью' },
+  { text: 'Публикация', disabled: !Boolean(username.value && comment.value) },
+])
 
 const hero = ref<HotsHero | undefined>(undefined)
 
@@ -22,27 +27,32 @@ const roleToTitleMap: Record<HotsHeroRole, string> = {
   [HotsHeroRole.Healer]: 'Хил',
   [HotsHeroRole.Support]: 'Саппорт',
 }
+
+const username = ref('')
+const comment = ref('')
+const rating = ref(0)
 </script>
 
 <template>
-  <v-tabs v-model="tabIndex" color="cyan" align-tabs="center">
-    <v-tab v-for="(title, index) of tabTitles" :value="index">{{ title }}</v-tab>
+  <v-tabs v-model="tabIndex" color="cyan" align-tabs="center" class="hots-post-creation-tabs">
+    <v-tab v-for="item in tabItems" :key="item.text" v-bind="item" />
   </v-tabs>
+
   <v-window v-model="tabIndex">
     <v-window-item :value="0">
-      <v-container>
-        <v-container class="hots-post-create-page">
-          <div v-for="role in HotsHeroRole" :key="role" class="hero-selection-section">
-            <h2>{{ roleToTitleMap[role] }}</h2>
-            <hots-hero-selector :role="role" :selected="hero" @select="select" />
-          </div>
-        </v-container>
+      <v-container class="hots-post-creation-container">
+        <div v-for="role in HotsHeroRole" :key="role" class="hero-selection-section">
+          <h2>{{ roleToTitleMap[role] }}</h2>
+          <hots-hero-selector :role="role" :selected="hero" @select="select" />
+        </div>
       </v-container>
     </v-window-item>
 
     <v-window-item :value="1">
-      <v-container>
-
+      <v-container class="hots-post-creation-container">
+        <v-text-field v-model="username" label="Имя дебила" />
+        <v-textarea v-model="comment" label="Отзыв" />
+        <v-rating v-model="rating" :color="hotsRatingColors[rating]" class="mx-auto" />
       </v-container>
     </v-window-item>
 
@@ -55,7 +65,11 @@ const roleToTitleMap: Record<HotsHeroRole, string> = {
 </template>
 
 <style scoped lang="scss">
-.hots-post-create-page {
+.hots-post-creation-tabs {
+  margin-bottom: 16px;
+}
+
+.hots-post-creation-container {
   display: flex;
   flex-direction: column;
   gap: 16px;
