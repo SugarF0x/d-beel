2fd9e45ee94d2c4093878db46bd9710e386e9b88
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-08-10 21:01:06 +0300

    fix reaction table column names

--- server/ydb/tables/hots_post_reaction.ts
+++ server/ydb/tables/hots_post_reaction.ts
@@ -2,16 +2,16 @@
 export const createHotsPostReactionTableQuery = `
 CREATE TABLE hots_post_reaction
 (
-    post_created_by Utf8 NOT NULL,
+    post_username Utf8 NOT NULL,
     post_created_at Datetime NOT NULL,
     created_by Utf8 NOT NULL,
     shortcut Utf8 NOT NULL,
-    PRIMARY KEY (post_created_by, post_created_at, created_by, shortcut)
+    PRIMARY KEY (post_username, post_created_at, created_by, shortcut)
 );
 `
 
 export interface HotsPostReactionRow {
-  post_created_by: string
+  post_username: string
   post_created_at: string
   created_by: string
   shortcut: string
