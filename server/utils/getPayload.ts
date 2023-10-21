73cbcdf0b28a6402137bc01545d001c2ef9b6a62
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-10-21 12:12:37 +0300

    fix payload getter transform not working

--- server/utils/getPayload.ts
+++ server/utils/getPayload.ts
@@ -4,12 +4,14 @@ import { z } from "zod"
 
 export async function getBodyPayload<T extends ZodObject<any>>(event: H3Event, schema: T): Promise<z.infer<T>> {
   const body = await readBody(event)
-  if (!schema.safeParse(body).success) throw createError({ statusCode: 400 })
-  return body as z.infer<T>
+  const zod = schema.safeParse(body)
+  if (!zod.success) throw createError({ statusCode: 400 })
+  return zod.data
 }
 
 export function getQueryPayload<T extends ZodObject<any>>(event: H3Event, schema: T): z.infer<T> {
   const query = getQuery(event)
-  if (!schema.safeParse(query).success) throw createError({ statusCode: 400 })
-  return query as z.infer<T>
+  const zod = schema.safeParse(query)
+  if (!zod.success) throw createError({ statusCode: 400 })
+  return zod.data
 }
