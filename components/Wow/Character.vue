e208a0e3bcfcd89499b0f423c57e6677f216bb4e
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-10-24 16:00:51 +0300

    adjust realm selector to bind key instead of localized value

---
+++ components/Wow/Character.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { WowCharacterProfileResponse, WowCharacterMediaResponse } from "~/server/ydb/types/wow/blizzApiResponse"
+
+const props = defineProps<{ profile:  }>()
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- components/Wow/RealmSelector.vue
+++ components/Wow/RealmSelector.vue
@@ -4,11 +4,14 @@ const modelValue = defineModel<string | undefined>()
 const { data, pending } = useFetch('/api/wow/realms')
 
 const items = computed(() => Object.keys(data.value ?? {}).sort())
+const value = ref<undefined | string>(undefined)
+
+watch(value, newValue => { modelValue.value = !newValue ? newValue : data.value?.[newValue] })
 </script>
 
 <template>
   <v-autocomplete
-    v-model="modelValue"
+    v-model="value"
     label="Сервер"
     :loading="pending"
     :disabled="pending"
