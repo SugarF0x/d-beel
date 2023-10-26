86d8866505f3f7007c9680adf4c67e494962dbfc
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-10-26 14:14:14 +0300

    fix realms selector to two-way-bind slug instead of title

--- components/Wow/RealmSelector.vue
+++ components/Wow/RealmSelector.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
-const modelValue = defineModel<string | undefined>()
+const modelValue = defineModel<string | null>()
 
 const { data, pending } = useFetch('/api/wow/realms')
 
-const items = computed(() => Object.keys(data.value ?? {}).sort())
-const value = ref<undefined | string>(undefined)
+type Item = {
+  title: string
+  value: string
+}
 
-watch(value, newValue => { modelValue.value = !newValue ? newValue : data.value?.[newValue] })
+const items = computed<Item[]>(() => {
+  const value = data.value
+  if (!value) return []
+
+  const keys = Object.keys(value).sort()
+  return keys.reduce<Item[]>((acc, key) => {
+    acc.push({ title: value[key], value: key })
+    return acc
+  }, [])
+})
 </script>
 
 <template>
   <v-autocomplete
-    v-model="value"
+    v-model="modelValue"
     label="Сервер"
     :loading="pending"
     :disabled="pending"
--- pages/wow/index.vue
+++ pages/wow/index.vue
@@ -5,8 +5,8 @@ const names = [
   'Лапковость',
 ]
 
-const name = ref('')
-const realm = ref<undefined | string>()
+const name = ref<null | string>(null)
+const realm = ref<null | string>(null)
 
 const { data, execute, status } = useAsyncData('character', () => $fetch('/api/wow/characters', { query: { characterName: name.value, realmSlug: realm.value } }), { immediate: false })
 const disabled = computed(() => status.value === 'pending' || !name.value || !realm.value)
@@ -20,10 +20,15 @@ const disabled = computed(() => status.value === 'pending' || !name.value || !re
       </v-btn>
     </div>
 
-    <v-text-field v-model="name" />
-    <wow-realm-selector v-model="realm" />
+    <div class="flex gap-4 mt-4">
+      <v-text-field v-model="name" label="Имя" />
+      <wow-realm-selector v-model="realm" />
+    </div>
+
     <v-btn @click="execute" :disabled="disabled">Найти</v-btn>
 
-    <wow-profile v-if="data" :media="data.media" :profile="data.profile" />
+    <div class="mt-4">
+      <wow-profile v-if="data" :media="data.media" :profile="data.profile" />
+    </div>
   </div>
 </template>
