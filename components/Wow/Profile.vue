1ded79ea1acbccdd2a83af0380a82f99c3215b55
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-10-26 20:03:06 +0300

    add wow character profile card base

---
+++ components/Wow/Profile.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { WowCharacterMedia, WowCharacterProfile } from "~/server/api/wow/characters.get"
+
+const props = defineProps<{ profile: WowCharacterProfile, media: WowCharacterMedia }>()
+</script>
+
+<template>
+  <v-card class="wow-profile-card">
+    <v-img :src="props.media.header" width="230" />
+
+    <pre>{{ profile }}</pre>
+  </v-card>
+</template>
+
+<style scoped lang="scss">
+.wow-profile-card {
+  // TODO: remove this, its temporary (probably)
+  max-width: 690px;
+}
+</style>
