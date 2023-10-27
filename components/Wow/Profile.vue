23ced4f7cce54a328e63486defed9ce510b23bed
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-10-27 15:19:24 +0300

    style profile card a bit

--- components/Wow/Profile.vue
+++ components/Wow/Profile.vue
@@ -2,13 +2,23 @@
 import type { WowCharacterMedia, WowCharacterProfile } from "~/server/api/wow/characters.get"
 
 const props = defineProps<{ profile: WowCharacterProfile, media: WowCharacterMedia }>()
+
+const factionToSlugMap: Record<string, string> = { Альянс: 'alliance', Орда: 'horde' }
 </script>
 
 <template>
   <v-card class="wow-profile-card">
-    <v-img :src="props.media.header" width="230" />
+    <v-img :src="props.media.fullSize" class="image" />
 
-    <pre>{{ profile }}</pre>
+    <v-divider vertical />
+
+    <div class="content">
+      <v-img :src="`/img/wow/factions/${factionToSlugMap[props.profile.faction]}/logo.png`" class="faction-logo" />
+
+      <h3>{{ profile.fullName }}</h3>
+      <h5>{{ profile.level }} (ilvl {{ profile.itemLevel }}) {{ profile.race }} {{ profile.class }} ({{ profile.spec }})</h5>
+      <h6 v-if="profile.guild">&lt;{{ profile.guild }}&gt;</h6>
+    </div>
   </v-card>
 </template>
 
@@ -16,5 +26,25 @@ const props = defineProps<{ profile: WowCharacterProfile, media: WowCharacterMed
 .wow-profile-card {
   // TODO: remove this, its temporary (probably)
   max-width: 690px;
+  display: flex;
+}
+
+.image {
+  transform: scale(4);
+  aspect-ratio: .5;
+  flex: .5;
+}
+
+.content {
+  flex: 1;
+  padding: 8px;
+}
+
+.faction-logo {
+  width: 64px;
+  height: 64px;
+  position: absolute;
+  top: 8px;
+  right: 8px;
 }
 </style>
