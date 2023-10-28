44fd677074075b17e1aa54d0e83a384b3bdd7fa4
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-10-28 18:57:11 +0300

    add wow post table item

--- server/ydb/tables/wow_post.ts
+++ server/ydb/tables/wow_post.ts
@@ -1,5 +1,52 @@
+import { WowCharacterMediaResponse, WowCharacterProfileResponse } from "~/server/ydb/types/wow/blizzApiResponse"
+
 export enum WowRating {
   GOOD = 'GOOD',
   BAD = 'BAD',
   NEUTRAL = 'NEUTRAL',
+}
+
+export enum WowEncounter {
+  KEY = 'KEY',
+  RAID = 'RAID',
+  BATTLEGROUND = 'BATTLEGROUND',
+  ARENA = 'ARENA',
+  WORLD = 'WORLD',
+  OTHER = 'OTHER',
+}
+
+export const createWowPostTableQuery = `
+CREATE TABLE wow_post
+(
+    name Utf8 NOT NULL,
+    realm Utf8 NOT NULL,
+    created_at Datetime NOT NULL,
+    
+    id Uint32,
+    profile Json,
+    media Json,
+    
+    created_by Utf8,
+    rating Utf8,
+    comment Utf8,
+    
+    encounter Utf8,
+    encounter_details Utf8,
+    
+    PRIMARY KEY (username, realm, created_at)
+);
+`
+
+export interface WowPostRow {
+  name: string
+  realm: string
+  created_at: string
+  id: number
+  profile: WowCharacterProfileResponse
+  media: WowCharacterMediaResponse
+  created_by: string
+  rating: WowRating
+  comment: string
+  encounter: WowEncounter
+  encounter_details: string
 }
