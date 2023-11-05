1bfadc57e4c0521a46d21101bf7f9dd1e77bc80c
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-11-05 22:59:22 +0300

    add potential todo

--- plugins/masonry.client.ts
+++ plugins/masonry.client.ts
@@ -1,5 +1,9 @@
 import { VueMasonryPlugin } from 'vue-masonry'
 
+// TODO: make a plugin to create web-types file with directives on demand? question mark?
+//   e.g. add to Plugin object a new entity 'webTypes' with .directive.add() method that
+//   would collect all the directives from all possible plugins and shove them all into the ./web-types.json
+
 export default defineNuxtPlugin(({ vueApp }) => {
   vueApp.use(VueMasonryPlugin)
 })
