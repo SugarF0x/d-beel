c36def381f78ec3697bfe4bc688dfa61241fef06
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-11-16 20:33:28 +0300

    add user page

--- pages/profile/[username].vue
+++ pages/profile/[username].vue
@@ -1,14 +1,53 @@
 <script setup lang="ts">
+import { format } from "date-fns"
+import { computed } from '#imports'
+
 const route = useRoute()
-const username = route.params.username
+const username = computed(() => String(route.params.username))
 
+const { pending, error, data } = useFetch(`/api/user/info`, { params: { username } })
+const info = computed<{ totalPosts: number, joinedAt: string }>(() => {
+  if (!data.value || (data.value && 'statusCode' in data.value)) return { totalPosts: 0, joinedAt: '' }
+  return data.value
+})
 
+const title = computed(() => (error.value?.statusCode ? String(error.value.statusCode) + ': ' : '') + username.value)
+useHead({ title })
 </script>
 
 <template>
-  {{ username }}
+  <v-container class="wrapper">
+    <v-skeleton-loader
+      :loading="pending"
+      type="list-item-two-line"
+      class="skeleton"
+    >
+      <div v-if="error && error.statusCode === 404" class="not-found">
+        <h1>Пользователь не найден</h1>
+      </div>
+      <v-list-item v-else :title="username">
+        <div>Количество постов: {{ info.totalPosts }}</div>
+        <div>Присоединился: {{ format(new Date(info.joinedAt), 'dd.MM.yyyy') }}</div>
+      </v-list-item>
+    </v-skeleton-loader>
+  </v-container>
 </template>
 
 <style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
+.skeleton {
+  min-width: 200px;
+}
+
+.not-found {
+  padding: 8px;
+  margin: 0 auto;
+}
 </style>
--- server/api/user/info.get.ts
+++ server/api/user/info.get.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 import { TypedValues } from "ydb-sdk"
+import { UserTableRow } from "~/server/ydb/tables/user"
 
 const { utf8 } = TypedValues
 
@@ -20,26 +21,34 @@ export default defineEventHandler(async (event) => {
     username: z.string().min(3).max(32)
   }))
 
-  const totalPosts = await useYDBSession(async session => {
-    const response = await ydbGet<{ total_posts: number }>(session, `
+  return await useYDBSession(async session => {
+    const [user] = await ydbGet<{ joinedAt: Date }>(session, `
+      DECLARE $username AS Utf8;
+      
+      SELECT created_at AS joinedAt
+      FROM user
+      WHERE username = $username; 
+    `, {
+      $username: utf8(username)
+    })
+
+    if (!user) return createError({ status: 404 })
+
+    const [{ totalPosts }] = await ydbGet<{ totalPosts: number }>(session, `
       DECLARE $author AS Utf8;
 
       SELECT SUM(total_posts)
-      AS total_posts 
+      AS totalPosts 
       FROM (
         ${tablesToCount.map(getTableCountQuery).join(' UNION ALL ')}
       );
     `, {
-      "$author": utf8(username)
+      $author: utf8(username)
     })
 
-    const results = response[0]?.total_posts
-    if (results === undefined) throw new Error('Malformed query response: total posts missing')
-
-    return results
+    return {
+      totalPosts,
+      joinedAt: user.joinedAt
+    }
   })
-
-  return {
-    totalPosts
-  }
 })
