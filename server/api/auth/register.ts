e5888489273e1db6b329b6f3ed9b3047750b0767
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-11-17 12:09:14 +0300

    add invite key validation query

--- server/api/auth/register.ts
+++ server/api/auth/register.ts
@@ -1,11 +1,20 @@
 import { TypedData, TypedValues } from "ydb-sdk"
 import { z } from 'zod'
 
+const { utf8, datetime, uint16 } = TypedValues
+
 export default defineEventHandler(async (event) => {
   const { username, password } = await getBodyPayload(event, z.object({
     username: z.string().min(1),
     password: z.string().min(1),
   }))
+
+  /*
+    TODO:
+      1. validate invite
+      2. validate name is available
+      3. create user & claim invite
+   */
 
   if (await doesUserAlreadyExist(username)) throw createError({ statusCode: 409 })
 
@@ -14,6 +23,23 @@ export default defineEventHandler(async (event) => {
   return true
 })
 
+async function validateInvite(key: string): Promise<boolean> | never {
+  return useYDBSession(async session => {
+    const [{ valid }] = await ydbGet<{ valid: boolean }>(session, `
+      DECLARE $key AS Utf8;
+      
+      SELECT CAST(COUNT(*) AS Bool)
+      AS valid
+      FROM user_invite 
+      WHERE key = $key AND claimed_by IS NULL;
+    `, {
+      $key: utf8(key)
+    })
+
+    return valid
+  })
+}
+
 async function doesUserAlreadyExist(username: string): Promise<boolean> {
   return await useYDBSession(async (session) => {
     const { resultSets } = await session.executeQuery(`
@@ -21,7 +47,7 @@ async function doesUserAlreadyExist(username: string): Promise<boolean> {
       
       SELECT COUNT(username) as count FROM user WHERE username = $username LIMIT 1;
     `, {
-      "$username": TypedValues.utf8(username)
+      "$username": utf8(username)
     })
 
     return Boolean(TypedData.createNativeObjects(resultSets[0])[0].count)
@@ -32,7 +58,7 @@ async function createUser(username: string, password: string) {
   const { hash, salt, iterations } = await generateHashPassword(password)
 
   await useYDBSession(async (session) => {
-    await session.executeQuery(`
+    await ydbPost(session, `
       DECLARE $username AS Utf8;
       DECLARE $created_at AS Datetime;
       DECLARE $hash AS Utf8;
@@ -42,11 +68,11 @@ async function createUser(username: string, password: string) {
       INSERT INTO user (username, created_at, hash, salt, iterations) 
       VALUES ($username, $created_at, $hash, $salt, $iterations);
     `, {
-      "$username": TypedValues.utf8(username),
-      "$created_at": TypedValues.datetime(new Date()),
-      "$hash": TypedValues.utf8(hash),
-      "$salt": TypedValues.utf8(salt),
-      "$iterations": TypedValues.uint16(iterations),
+      "$username": utf8(username),
+      "$created_at": datetime(new Date()),
+      "$hash": utf8(hash),
+      "$salt": utf8(salt),
+      "$iterations": uint16(iterations),
     })
   })
 }
