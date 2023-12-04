d907861766f0fb0f9a84252bedb0edc63640dd2e
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-12-04 21:43:01 +0300

    use nanoid for invite key generation

--- server/api/auth/invite-code.post.ts
+++ server/api/auth/invite-code.post.ts
@@ -1,13 +1,17 @@
 import { TypedValues } from "ydb-sdk"
-import { randomUUID } from "crypto"
+import { customAlphabet } from "nanoid"
+import { chunk } from "lodash-es"
 
 const { utf8, datetime } = TypedValues
+
+const uuid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 12)
+const generateKey = () => chunk(uuid(), 4).map(e => e.join('')).join('-')
 
 export default defineEventHandler(async (event) => {
   const { user: { username: created_by } } = await useServerAuth(event)
 
   return await useYDBSession(async session => {
-    const key = randomUUID()
+    const key = generateKey()
 
     await ydbPost(session, `
       DECLARE $key AS Utf8;
