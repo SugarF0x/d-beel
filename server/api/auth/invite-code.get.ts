ad753a1888ab2d0091543b7893f616b22d726f0e
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-12-04 21:43:45 +0300

    add endpoint to retrieve user created tokens

---
+++ server/api/auth/invite-code.get.ts
@@ -0,0 +1,19 @@
+import { TypedValues } from "ydb-sdk"
+
+const { utf8 } = TypedValues
+
+export default defineEventHandler(async (event) => {
+  const { user: { username: created_by } } = await useServerAuth(event)
+
+  return await useYDBSession(async session => (
+    await ydbGet(session, `
+      DECLARE $created_by AS Utf8;
+
+      SELECT key, claimed_by
+      FROM user_invite
+      WHERE created_by = $created_by;
+    `, {
+      $created_by: utf8(created_by)
+    })
+  ))
+})
