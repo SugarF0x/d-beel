2a571e574b17323ea33f824f29dcf82c79295472
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-12-08 18:17:17 +0300

    restrict new key creating to users of one month or older

--- server/api/user/invite-code.post.ts
+++ server/api/user/invite-code.post.ts
@@ -1,4 +1,4 @@
-import { TypedValues } from "ydb-sdk"
+import { Session, TypedValues } from "ydb-sdk"
 import { customAlphabet } from "nanoid"
 import { chunk } from "lodash-es"
 
@@ -8,9 +8,12 @@ const uuid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 12)
 const generateKey = () => chunk(uuid(), 4).map(e => e.join('')).join('-')
 
 export default defineEventHandler(async (event) => {
-  const { user: { username: created_by } } = await useServerAuth(event)
+  const { user: { username } } = await useServerAuth(event)
 
   return await useYDBSession(async session => {
+    const isValid = await getCanAccountCreateNewKeys(session, username)
+    if (!isValid) throw createError({ statusCode: 403, message: "Создавать ключи могут только пользователи старше одного месяца" })
+
     const key = generateKey()
 
     await ydbPost(session, `
@@ -22,10 +25,27 @@ export default defineEventHandler(async (event) => {
       VALUES ($key, $created_by, $created_at);
     `, {
       $key: utf8(key),
-      $created_by: utf8(created_by),
+      $created_by: utf8(username),
       $created_at: datetime(new Date())
     })
 
     return key
   })
 })
+
+async function getCanAccountCreateNewKeys(session: Session, username: string) {
+  const MINIMUM_MONTHS_AGE = 1
+
+  const [{ valid }] = await ydbGet<{ valid: boolean }>(session, `
+    DECLARE $username AS Utf8;
+    
+    SELECT CAST(COUNT(*) AS Bool)
+    FROM user
+    AS valid
+    WHERE username = $username AND DateTime::MakeDate(DateTime::ShiftMonths(DateTime::Split(created_at), ${MINIMUM_MONTHS_AGE})) < CurrentUtcDatetime();
+  `, {
+    $username: utf8(username)
+  })
+
+  return valid
+}
